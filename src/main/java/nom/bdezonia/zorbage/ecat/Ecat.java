9725dddc256e83fe5328dca542cd15f29c6cfbd2
Barry DeZonia <bdezonia@gmail.com>
2022-06-11 20:43:03 -0500

    Update copyrights

--- src/main/java/nom/bdezonia/zorbage/ecat/Ecat.java
+++ src/main/java/nom/bdezonia/zorbage/ecat/Ecat.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2021 Barry DeZonia
+  Copyright (C) 2021-2022 Barry DeZonia
 
   This library is free software; you can redistribute it and/or modify it
   under the terms of the GNU Lesser General Public License as published by
